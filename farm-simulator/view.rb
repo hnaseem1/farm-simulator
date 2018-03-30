5f1f9aa26e9d84e0894e663c671c0189dc52e2b6
Hasan Naseem <hn92@live.com>
2018-03-30 15:53:30 -0400

    comments added to view

--- farm-simulator/view.rb
+++ farm-simulator/view.rb
@@ -1,15 +1,17 @@
 require_relative 'farm'
 
+# view displays and computes values for the the main program
 class View < Field
 
-
-  def display_harvested_food
+# displays the total harvest using Farm class
+  def self.display_harvested_food
 
     harvested_food = Farm.all_food
     puts "The Farm has #{harvested_food} harvested food so far"
   end
 
-  def relax
+# displays an informative msg using Fields class @@area hash
+  def self.relax
     Field.area.each do |k, v|
 
       if k == "corn"
@@ -19,13 +21,15 @@ class View < Field
         "The sun hangs low, casting an orange glow on a sea of #{v} hectares of wheat."
   end
 
-  def status
+# Displays total harvest for each type of field
+  def self.status
     Field.area.each do |k, v|
       puts "#{k} field is #{v} hectres"
       display_harvested_food
   end
 
-  def harvest
+# Computes and displays harvest for each field instance
+  def self.harvest
 
     Field.fields.each do |field|
 
